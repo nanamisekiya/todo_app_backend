8b1f99b91bf652ddd3e47bad3169fa2ae8bd1520
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-05-28 22:53:18 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks do
+    member do
+      post 'start_timer'
+      patch 'stop_timer'
+    end
+  end
 end
